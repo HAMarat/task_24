44aa71de35994f064df357bc50908f5ce10fd864
HAMarat <smile56680@gmail.com>
2023-03-04 14:18:29 +0300

    feat: HM finish

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import os
 
-from flask import Flask
+from flask import Flask, request, jsonify
+from marshmallow import ValidationError
+
+from models import RequestSchema
+from builder import build_query
 
 app = Flask(__name__)
 
@@ -8,10 +12,31 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 
 
-@app.route("/perform_query")
+@app.route("/perform_query", methods=['POST'])
 def perform_query():
-    # получить параметры query и file_name из request.args, при ошибке вернуть ошибку 400
-    # проверить, что файла file_name существует в папке DATA_DIR, при ошибке вернуть ошибку 400
-    # с помощью функционального программирования (функций filter, map), итераторов/генераторов сконструировать запрос
-    # вернуть пользователю сформированный результат
-    return app.response_class('', content_type="text/plain")
+
+    data_request = request.json
+
+    try:
+        RequestSchema().load(data_request)
+    except ValidationError as error:
+        return jsonify(error.messages), 400
+
+    file_name = data_request.get("file_name")
+    path = os.path.join(DATA_DIR, file_name)
+
+    first_result = build_query(
+        cmd=data_request["cmd1"],
+        value=data_request["value1"],
+        file_name=path,
+        data=None
+    )
+
+    second_result = build_query(
+        cmd=data_request["cmd2"],
+        value=data_request["value2"],
+        file_name=path,
+        data=first_result
+    )
+
+    return jsonify(second_result)
